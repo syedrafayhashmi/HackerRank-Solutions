6dfdb889a45be8b09f963309eaab13769fadb27b
syedrafayhashmi <rafaysidsid@gmail.com>
2020-07-24 12:06:04 +0500

    twosum

---
+++ LeetCode/twosum.py
@@ -0,0 +1,13 @@
+# Given nums = [2, 7, 11, 15], target = 9,
+
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
